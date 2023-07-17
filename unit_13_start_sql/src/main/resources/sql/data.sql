aa6a7e1bedda0aed084e8fd6ec2a038ef0826281
egorfuntusov <funtushan@gmail.com>
2023-07-17 21:46:57 +0300

    start sql

--- unit_13_start_sql/src/main/resources/sql/data.sql
+++ unit_13_start_sql/src/main/resources/sql/data.sql
@@ -1,3 +1,4 @@
+-- create
 insert into departments values (default, 'JAVA');
 insert into departments values (default, 'JS');
 insert into departments values (default, 'PYTHON');
@@ -6,3 +7,15 @@ insert into employees values (default, 'qq', 'qqq', 20);
 insert into employees values (default, 'ww', 'www', 25);
 insert into employees values (default, 'ee', 'eee', 28);
 insert into employees values (default, 'rr', 'rrr', 38);
+
+-- update
+update employees set first_name = 'fsf', last_name = 'dfsafsa', age = 10 where id = 1;
+
+-- delete
+delete from employees where id = 1;
+
+
+-- read
+select * from employees;
+select * from employees where id = 12;
+
