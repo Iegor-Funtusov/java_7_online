82d61f6263794d22d8176704899bda43f8be8171
egorfuntusov <funtushan@gmail.com>
2023-08-17 21:55:41 +0300

    about Spring data

--- unit_18_spring_data/src/main/java/ua/com/alevel/repository/EmployeeRepository.java
+++ unit_18_spring_data/src/main/java/ua/com/alevel/repository/EmployeeRepository.java
@@ -1,6 +1,8 @@
 package ua.com.alevel.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ua.com.alevel.entity.Employee;
 
 import java.util.List;
@@ -8,4 +10,10 @@ import java.util.List;
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     List<Employee> findAllByFirstNameStartingWithIgnoreCaseOrLastNameEndingWithIgnoreCaseAndAgeBetween(String fn, String ln, int start, int end);
+
+    @Query("select e from Employee e where e.id not in (select e.id from Employee e join e.departments ed where ed.id = :depId)")
+    List<Employee> findAllEmployeesByExcludeDepartment(@Param("depId") Long depId);
+
+    @Query(value = "select * from employees as emp left join dep_emp as de on emp.id = de.emp_id where de.dep_id = ?", nativeQuery = true)
+    List<Employee> findAllEmployeesByDepartment(Long depId);
 }
